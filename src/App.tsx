b924a83f74032d662a056708873d923f1e894203
Yarden Porat <yardenporat@gmail.com>
2022-11-05 16:29:11 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ function App() {
 
     return (
         <div className={classes.app}>
-            123
             <div className={classes.inputs}>
                 <Autocomplete<Option>
                     id="country-code"
